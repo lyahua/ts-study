fb8eead02f9c7bc87a4c36830b08e5483f362c50
liuyahua <1251271810@qq.com>
2024-11-19 23:41:20 +0800

    feat: in和进阶1

---
+++ src/views/day04in/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+type U = 'a' | 'b' |'c';
+type Foo =  {
+    [key in U] : string
+}
+
+type User = {
+    readonly id: number,
+    name: string,
+    tel: string;
+    address?: string
+}
+type CopyUser = {
+    [key in keyof User]: User[key]
+}
+
+// 增加泛型
+type MyCopy<T> = {
+    [key in keyof T]: T[key]
+}
+
+type ACopyt = MyCopy<User>
+const A:ACopyt = {
+    id: 123,
+    name: '123',
+    tel: '123'
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/day05进阶1/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+type MyCopy<T> = {
+    [key in keyof T]: T[key]
+}
+
+// 变成只读属性
+type MyReadonly<T> = {
+   readonly [key in keyof T]: T[key]
+}
+
+// 变成可靠属性
+type MyPartial<T> = {
+    [key in keyof T]?: T[key]
+}
+
+
+
+</script>
+
+<style scoped>
+
+</style>
