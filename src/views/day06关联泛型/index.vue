24a1d6da63a20f95353567ef64f11c1e4aeb6026
liuyahua <1251271810@qq.com>
2024-11-25 22:50:49 +0800

    2

---
+++ src/views/day06关联泛型/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+type MyPick<T, U extends keyof T> ={
+    [key in U]:T[key]
+}
+
+type User = {
+    name: string,
+    age: number,
+    tel?:string
+}
+type OneUser = MyPick<User, 'name'| 'age'>
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/day07方括号运算符常见操作/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+/**获取值 的类型 */
+type User = {
+    readonly id:number,
+    name: string,
+    tel: string,
+    address?:string
+}
+type ValueType = User[keyof User]
+// 泛型
+type MyReadOnly<T> = {
+    +readonly [key in keyof T]: T[key]
+}
+
+/**数组一样可以处理 */
+const arr = ['admin', 'user', 'client']
+type ArrTYpe =typeof arr[number]
+
+/**将数组通过as const转为元组类型后，得到的是具体的字面量类型的联合 */
+const arrUqine = ['admin', 'user', 'client'] as const;
+type UqineType = typeof arrUqine[number]
+
+const students = [
+    {name: '1', age: 2, is: true},
+    {name: '2', age: 3, is: false}
+]
+type StudentType = typeof students[number]
+type Res = StudentType[keyof StudentType]
+
+</script>
+
+<style scoped>
+
+</style>
